3a47828f770ca7f3971de1297fad11249192e611
emirtepedeldiren <emir.tepedeldiren@gmail.com>
2026-01-21 16:40:59 +0300

    voice bug fixed

--- yt_downloader.py
+++ yt_downloader.py
@@ -1,9 +1,13 @@
 from pytubefix import YouTube
 
 url = input("Enter video URL: ")
-
 path = ""
 
-YouTube(url).streams.get_highest_resolution().download(path)
+yt = YouTube(url)
+
+stream = yt.streams.filter(progressive=True, file_extension='mp4').get_highest_resolution()
+
+stream.download(path)
+
 
 # ! Wherever you save this project, it will store the videos you download in that folder.!
