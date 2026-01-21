e2acfde6182db80f3cdf2321132d50ac5540e8fc
emirtepedeldiren <emir.tepedeldiren@gmail.com>
2026-01-21 12:59:21 +0300

    YouTube downloader project

--- yt_downloader.py
+++ yt_downloader.py
@@ -6,4 +6,4 @@ path = ""
 
 YouTube(url).streams.get_highest_resolution().download(path)
 
-##Wherever you save this project, it will store the videos you download in that folder.
+# !Wherever you save this project, it will store the videos you download in that folder.!
